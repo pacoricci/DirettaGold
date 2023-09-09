e610d8e50d078f74cfda995738400860bf3ea016
Paco Ricci <paco.ricci@gmail.com>
2023-09-10 01:36:47 +0200

    Jquery removed

--- direttagold.js
+++ direttagold.js
@@ -1,23 +1,4 @@
 /**
- * This function sends a request to www.goldebet.it and receives a JSON response.
- * The request is based on the text entered in the search bar of https://www.goldbet.it/scommesse/
- * @param {string} text The query text for the search
- * @returns {JSON} A JSON object containing a list of matching results
- */
-function searchGold(text) {
-    return $.ajax({
-        url: 'https://www.goldbet.it/scommesse/searchEvents/',
-        data: '{"strSearch":"' + text + '"}',
-        headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json'
-        },
-        type: 'Post',
-        async: false,
-        dataType: 'json'
-    }).responseJSON;
-}
-/**
  * This function extracts the “ei” attribute from each element of the input JSON array and returns them as a new array.
  * @param {Object} a The input JSON array
  * @returns {Array<string>} An array of strings representing the “ei” values
@@ -29,6 +10,7 @@ function jsonToEiArray(a) {
     }
     return result;
 }
+
 /**
  * This function verifies if the data in the input JSON object matches the data on diretta.it
  * @param {JSON} nodo The input JSON object containing the data to check
@@ -41,13 +23,14 @@ function check(nodo) {
     equalData = (direttaData === goldData);
     return thereisntSpeciali && equalData;
 }
+
 /**
  * This function adds a CSS style to display the fastcode to the user 
  */
-function cssAppendFastCode() {
-    const myStyle = document.createElement('style');
-    myStyle.innerHTML = `
-      .prematchLink[title="GoldBet"]::after {
+function cssAppendFastCode(oc) {
+    const innerHTML = `
+    <style>
+        .prematchLink[title="GoldBet"]::after {
         content: "Fastcode: ${oc}";
         font-size: 16px;
         text-indent: 30px;
@@ -56,46 +39,71 @@ function cssAppendFastCode() {
         align-items: center;
         white-space: nowrap;
       }
+      </style> 
     `;
-    document.head.appendChild(myStyle);
+    const parser = new DOMParser();
+    const parsed = parser.parseFromString(innerHTML, 'text/html');
+    const myStyle = parsed.querySelector('style');
+    document.querySelector('head').appendChild(myStyle);
 }
+
 function init() {
+    var counter = 0;
     const participantInterval = setInterval(() => {
+        counter++;
+        if (counter > 50) {
+            return 0
+        }
         if (document.querySelectorAll('a.participant__overflow').length === 2) {
             clearInterval(participantInterval);
             second()
         }
     }, 100);
-    function second() {
-        const aTeams = document.querySelectorAll('a.participant__overflow');
-        const firstTeam = aTeams[0].firstChild.textContent.toLowerCase();
-        const secondTeam = aTeams[1].firstChild.textContent.toLowerCase();
-        const firstTeamSearch = searchGold(firstTeam);
-        const secondTeamSearch = searchGold(secondTeam);
-        console.log(typeof firstTeamSearch);
-        // Arrays intesection
-        const firstEiArray = jsonToEiArray(firstTeamSearch);
-        const secondEiArray = jsonToEiArray(secondTeamSearch);
-        const intersectionArray = [];
-        for (let i = 0; i < firstEiArray.length; i++) {
-            if (secondEiArray.includes(firstEiArray[i])) intersectionArray.push(i);
-        }
-        var i = 0;
-        var node = firstTeamSearch[intersectionArray[0]];
-        while (!check(node) && i < intersectionArray.length) {
-            i++;
-            if (i >= intersectionArray.length) {
-                return 0;
-            }
-            node = firstTeamSearch[intersectionArray[i]];
-        }
-        oc = node.oc;
-        cssAppendFastCode();
-    }
 }
 
+function second() {
+    const aTeams = document.querySelectorAll('a.participant__overflow');
+    const firstTeam = aTeams[0].firstChild.textContent.toLowerCase();
+    const xhttp1 = new XMLHttpRequest();
+    xhttp1.onload = function () { third(xhttp1.response); };
+    xhttp1.open('POST', 'https://www.goldbet.it/scommesse/searchEvents/');
+    jsonRequest = JSON.stringify({
+        strSearch: firstTeam
+    });
+    xhttp1.send(jsonRequest);
+}
 
-var oc = 0;
-const goldQuery = 'a.prematchLink[title="GoldBet"]';
-console.log('Inizio');
+function third(firstResponse) {
+    const aTeams = document.querySelectorAll('a.participant__overflow');
+    const secondTeam = aTeams[1].firstChild.textContent.toLowerCase();
+    const xhttp2 = new XMLHttpRequest();
+    xhttp2.onload = function () { forth(firstResponse, xhttp2.response); };
+    xhttp2.open('POST', 'https://www.goldbet.it/scommesse/searchEvents/');
+    jsonRequest = JSON.stringify({
+        strSearch: secondTeam
+    });
+    xhttp2.send(jsonRequest);
+}
+
+function forth(firstResponse, secondResponse) {
+    const firstTeamSearch = JSON.parse(firstResponse);
+    const secondTeamSearch = JSON.parse(secondResponse);
+    const firstEiArray = jsonToEiArray(firstTeamSearch);
+    const secondEiArray = jsonToEiArray(secondTeamSearch);
+    const intersectionArray = [];
+    for (let i = 0; i < firstEiArray.length; i++) {
+        if (secondEiArray.includes(firstEiArray[i])) intersectionArray.push(i);
+    }
+    var i = 0;
+    var node = firstTeamSearch[intersectionArray[0]];
+    while (!check(node) && i < intersectionArray.length) {
+        i++;
+        if (i >= intersectionArray.length) {
+            return 0;
+        }
+        node = firstTeamSearch[intersectionArray[i]];
+    }
+    cssAppendFastCode(node.oc);
+}
+
 init();
